e90945548acf12d28f1284ef7d5755e8bd16dd3e
Aby Dzakiyyah <abydzakiyyah@gmail.com>
2021-12-05 05:30:24 +0800

    Update purchase module

--- module/appStorePurchase/appStorePurchase.php
+++ module/appStorePurchase/appStorePurchase.php
@@ -13,13 +13,13 @@ $msg_removed = "Berhasil Menghapus Data";
 
 switch ($p_act) {
     case "load_" . $p_slug:
-        $field = "IF(purchaseType='others',CONCAT(id,':hide'),id) id,purchaseName,purchaseRealDate,purchaseType,purchasePrice,purchaseStatus,iF(purchaseImport='on','<i class=\"fa fa-check-circle green\"></i>','<i class=\"fa fa-times-circle red\"></i>') purchaseImport";
+        $field = "id,purchaseName,purchaseRealDate,purchaseType,purchasePrice,purchaseStatus";
         $result = getDataP($conn, $field, $tablename, "purchaseName LIKE '%$p_search%' ORDER by purchaseDateTime DESC", $p_page, $p_perpage, true);
         echo json_encode($result);
         break;
 
     case "select_" . $p_slug:
-        $result = getData($conn, "pc.*,sp.supplierName", "purchase pc LEFT JOIN supplier sp ON pc.purchaseSupplierId=sp.id", "pc.id='$p_id'");
+        $result = getData($conn, "*", "purchase", "id='$p_id'");
         echo json_encode($result);
         break;
 
@@ -50,32 +50,7 @@ switch ($p_act) {
         }
         echo json_encode($result);
         break;
-    case "loadSupplier":
-        $result = getDataN($conn, "id,supplierName", "supplier", "supplierName LIKE '%$p_search%' LIMIT 10");
-        echo json_encode($result);
-        break;
 
-    case "download_format_xls":
-        require_once (sysclass."simplexlsxgen.class.php");
-        $purchase = getData($conn, "purchaseName,purchaseTaxValue,purchaseShipping", "purchase","id='$p_id' AND purchaseType='restock'");
-        $products = getDataN($conn, "pd.id,pd.productName,pc.priceCapitalPrice", "product pd LEFT JOIN price pc ON pc.priceProductId=pd.id");
-        $colfinish = count($products['data']);
-        $xlsx_data = [
-                        ['ID_REFERENSI', $p_id],
-                        ['PAJAK', $purchase['data']['purchaseTaxValue']],
-                        ['PENGIRIMAN', $purchase['data']['purchaseShipping']],
-                        ['TOTAL_HARGA', "=SUM(E7:E".(6+$colfinish).")"],
-                        ['FINAL_HARGA', "=SUM(B2:B4)"],
-                        ['ID_PRODUK', 'NAMA_PRODUK','QUANTITY','SATUAN_HARGA','SUB_TOTAL_HARGA']
-                    ];
-        $i=7;
-        foreach ($products['data'] as $product){
-            $xlsx_data[] = [$product['id'],$product['productName'],0,$product['priceCapitalPrice'],'=SUM(C'.$i.'*D'.$i.')'];
-            $i++;
-        }
-        $xlsx = SimpleXLSXGen::fromArray($xlsx_data);
-        $xlsx->downloadAs(slugConverter($purchase['data']['purchaseName']).".xlsx");
-        break;
     default:
         break;
 }
